93e32f8bd3d5852080b7980c76cfa3825bc5d73c
Wladyslaw Surala <>
2018-05-11 14:51:35 +0200

    displaying random numbers

--- Tachitoscope/ViewController.swift
+++ Tachitoscope/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonStart: UIButton!
+    @IBOutlet weak var labelTachitoscope: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func start(_ sender: Any) {
+        labelTachitoscope.alpha = 0
+        let numberOfDigits = 8
+        var stringTachited = ""
+        for _ in 1..<numberOfDigits {
+            stringTachited += randomDigit()
+        }
+        labelTachitoscope.text = stringTachited
+        labelTachitoscope.alpha = 1
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1 ) {
+            self.labelTachitoscope.alpha = 0
+        }
+        
+    }
+    
+    func randomDigit() -> String {
+        let digit = arc4random_uniform(10)
+        return "\(digit)"
+    }
+    
 }
 
